d5ca3ce909fdcf2a22b0eb3fad7ea7780bb0e401
djping <1938432787@qq.com>
2019-04-15 22:45:57 +0800

    source_map and hot module

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -7,7 +7,7 @@ function component() {
   var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Helllo', 'webpack'], ' ');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,9 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -16,7 +21,8 @@ module.exports = {
   output: {
     // filename: 'bundle.js',
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname,'dist')
+    path: path.resolve(__dirname,'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
